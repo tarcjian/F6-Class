51e06caeebfca4cf8099bd9d22c0d839cd87ddf3
B1 <B1@B142>
2017-11-01 13:07:58 +0800

    My first repository will mark the beginning of the endless repository to come.

--- src/practical/pkg2/Practical2.java
+++ src/practical/pkg2/Practical2.java
@@ -17,6 +17,7 @@ public class Practical2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("My Message");
     }
     
 }
---
+++ src/practical/pkg2/Temp.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical.pkg2;
+
+/**
+ *
+ * @author B1
+ */
+public class Temp {
+    
+}
